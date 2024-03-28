f70eb90b76f46034e22d1fe9b87f2ddedd956fcb
Jarrod Folino <109529620+JarrodFolinoFC@users.noreply.github.com>
2024-03-28 20:32:27 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Jarrod Folino
+===========
+
+```
+I write code.
+```
+
+### Connect with me
+* [github.com/JarrodFolinoFC][github]
+* [jdfolino@icloud.com][email]
+
---
+++ index.js
@@ -0,0 +1,13 @@
+function selfPromote() {
+  console.log('\n\n');
+  console.log('**********************************************');
+  console.log('**********************************************');
+  console.log('***************     JDF       ****************');
+  console.log('**********************************************');
+  console.log('**********************************************');
+  console.log('\n\n');
+}
+
+selfPromote();
+
+module.exports = selfPromote;
